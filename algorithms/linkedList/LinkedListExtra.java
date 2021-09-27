21b2d7575456f038e75fcb73b3bfe9b8443b00da
tatianaam <tatiana28am@gmail.com>
2021-09-27 11:22:08 -0400

    feat: sumOfTwoLists() modified

--- algorithms/linkedList/LinkedListExtra.java
+++ algorithms/linkedList/LinkedListExtra.java
@@ -2,21 +2,53 @@ package com.company;
 
 public class LinkedListExtra {
 
+    //returns null if length of two provided lists are not equal
     public static LinkedList sumOfTwoLists(LinkedList one, LinkedList two) {
         LinkedList sum = null;
+        Node currentOne = one.head;
+        Node currentTwo = two.head;
+        Node sumNode;
 
-        if(one.count() == two.count()) {
+        while (currentOne != null) {
+            if (currentTwo != null) {
+                sumNode = new Node(currentOne.value + currentTwo.value);
+                if (currentOne == one.head) {
+                    sum = new LinkedList();
+                    sum.head = sumNode;
+                } else {
+                    sum.tail.next = sumNode;
+                }
+                sum.tail = sumNode;
+                currentOne = currentOne.next;
+                currentTwo = currentTwo.next;
+            } else {
+                sum = null;
+                break;
+            }
+        }
+
+        if (currentTwo != null)
+            sum = null;
+
+        return sum;
+    }
+
+
+    //using count() and addInTail()
+    public static LinkedList sumOfTwoListsTwo(LinkedList one, LinkedList two) {
+        LinkedList sum = null;
+
+        if (one.count() == two.count()) {
             sum = new LinkedList();
             Node currentOne = one.head;
             Node currentTwo = two.head;
 
-            while(currentOne != null) {
+            while (currentOne != null) {
                 sum.addInTail(new Node(currentOne.value + currentTwo.value));
                 currentOne = currentOne.next;
                 currentTwo = currentTwo.next;
             }
         }
-
         return sum;
     }
 }
