63c51541313151b0f09a0c1d825085b99c3e1d6d
tatianaam <tatiana28am@gmail.com>
2021-12-08 23:02:20 -0500

    feat: binary search tree implementation

---
+++ algorithms/tree/BalancedBST.java
@@ -0,0 +1,66 @@
+package Tree;
+
+class BSTNode {
+    public int NodeKey;
+    public BSTNode Parent;
+    public BSTNode LeftChild;
+    public BSTNode RightChild;
+    public int Level;
+
+    public BSTNode(int key, BSTNode parent) {
+        NodeKey = key;
+        Parent = parent;
+        LeftChild = null;
+        RightChild = null;
+        if (parent != null)
+            Level = parent.Level + 1;
+    }
+}
+
+class BalancedBST {
+    public BSTNode Root;
+
+    public BalancedBST() {
+        Root = null;
+    }
+
+    public void GenerateTree(int[] a) {
+        this.Root = treeGeneratorHelper(a, this.Root, 0, a.length);
+    }
+
+    private BSTNode treeGeneratorHelper(int[] a, BSTNode node, int start, int end) {
+        if (start == end)
+            return null;
+        BSTNode temp = null;
+        if (end - start == 2 && a[start] == a[end - 1]) {
+            temp = new BSTNode(a[start], node);
+            temp.RightChild = new BSTNode(a[end - 1], temp);
+        } else {
+            int i = (end - start) / 2 + start;
+            temp = new BSTNode(a[i], node);
+            temp.LeftChild = treeGeneratorHelper(a, temp, start, i);
+            temp.RightChild = treeGeneratorHelper(a, temp, i + 1, end);
+        }
+        return temp;
+    }
+
+    public boolean IsBalanced(BSTNode root_node) {
+        if (root_node.RightChild != null) {
+            if (root_node.LeftChild != null)
+                return IsBalanced(root_node.RightChild) && IsBalanced(root_node.LeftChild);
+            else if (root_node.RightChild.LeftChild != null || root_node.RightChild.RightChild != null)
+                return false;
+            else
+                return true;
+        } else {
+            if (root_node.LeftChild != null)
+                if (root_node.LeftChild.LeftChild != null || root_node.LeftChild.RightChild != null)
+                    return false;
+                else
+                    return true;
+            else
+                return true;
+        }
+    }
+
+}
---
+++ algorithms/tree/BalancedBSTTest.java
@@ -0,0 +1,125 @@
+package Tree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class BalancedBSTTest {
+
+    @Test
+    public void testSingle() {
+        int[] input = {1};
+        BalancedBST balancedBST = new BalancedBST();
+        Assert.assertEquals(null, balancedBST.Root);
+        balancedBST.GenerateTree(input);
+        Assert.assertEquals(1, balancedBST.Root.NodeKey);
+        Assert.assertEquals(0, balancedBST.Root.Level);
+        Assert.assertEquals(null, balancedBST.Root.Parent);
+        Assert.assertEquals(null, balancedBST.Root.LeftChild);
+        Assert.assertEquals(null, balancedBST.Root.RightChild);
+        Assert.assertEquals(true, balancedBST.IsBalanced(balancedBST.Root));
+    }
+
+    @Test
+    public void testTwo() {
+        int[] input = {1, 2};
+        BalancedBST balancedBST = new BalancedBST();
+        Assert.assertEquals(null, balancedBST.Root);
+        balancedBST.GenerateTree(input);
+        Assert.assertEquals(2, balancedBST.Root.NodeKey);
+        Assert.assertEquals(0, balancedBST.Root.Level);
+        Assert.assertEquals(null, balancedBST.Root.Parent);
+        Assert.assertEquals(null, balancedBST.Root.RightChild);
+        Assert.assertEquals(1, balancedBST.Root.LeftChild.NodeKey);
+        Assert.assertEquals(1, balancedBST.Root.LeftChild.Level);
+        Assert.assertEquals(true, balancedBST.IsBalanced(balancedBST.Root));
+    }
+
+    @Test
+    public void testThree() {
+        int[] input = {1, 2, 3};
+        BalancedBST balancedBST = new BalancedBST();
+        Assert.assertEquals(null, balancedBST.Root);
+        balancedBST.GenerateTree(input);
+        Assert.assertEquals(2, balancedBST.Root.NodeKey);
+        Assert.assertEquals(0, balancedBST.Root.Level);
+        Assert.assertEquals(null, balancedBST.Root.Parent);
+        Assert.assertEquals(3, balancedBST.Root.RightChild.NodeKey);
+        Assert.assertEquals(1, balancedBST.Root.RightChild.Level);
+        Assert.assertEquals(1, balancedBST.Root.LeftChild.NodeKey);
+        Assert.assertEquals(1, balancedBST.Root.LeftChild.Level);
+        Assert.assertEquals(2, balancedBST.Root.LeftChild.Parent.NodeKey);
+        Assert.assertEquals(2, balancedBST.Root.RightChild.Parent.NodeKey);
+        Assert.assertEquals(true, balancedBST.IsBalanced(balancedBST.Root));
+    }
+
+    @Test
+    public void testFour() {
+        int[] input = {1, 2, 3, 5};
+        BalancedBST balancedBST = new BalancedBST();
+        Assert.assertEquals(null, balancedBST.Root);
+        balancedBST.GenerateTree(input);
+        Assert.assertEquals(1, balancedBST.Root.LeftChild.LeftChild.NodeKey);
+        Assert.assertEquals(true, balancedBST.IsBalanced(balancedBST.Root));
+    }
+
+
+    @Test
+    public void testFive() {
+        int[] input = {1, 2, 3, 5, 6};
+        BalancedBST balancedBST = new BalancedBST();
+        Assert.assertEquals(null, balancedBST.Root);
+        balancedBST.GenerateTree(input);
+        Assert.assertEquals(1, balancedBST.Root.LeftChild.LeftChild.NodeKey);
+        Assert.assertEquals(5, balancedBST.Root.RightChild.LeftChild.NodeKey);
+        Assert.assertEquals(true, balancedBST.IsBalanced(balancedBST.Root));
+    }
+
+    @Test
+    public void testMany() {
+        int[] input = {9, 18, 11, 6, 2, 19, 35, 25, 30, 17, 16, 20, 21, 22, 23};
+        Arrays.sort(input);
+        BalancedBST balancedBST = new BalancedBST();
+        Assert.assertEquals(null, balancedBST.Root);
+        balancedBST.GenerateTree(input);
+        Assert.assertEquals(19, balancedBST.Root.NodeKey);
+        Assert.assertEquals(2, balancedBST.Root.LeftChild.LeftChild.LeftChild.NodeKey);
+        Assert.assertEquals(35, balancedBST.Root.RightChild.RightChild.RightChild.NodeKey);
+        Assert.assertEquals(3, balancedBST.Root.LeftChild.LeftChild.LeftChild.Level);
+        Assert.assertEquals(3, balancedBST.Root.RightChild.RightChild.RightChild.Level);
+        Assert.assertEquals(22, balancedBST.Root.RightChild.LeftChild.RightChild.NodeKey);
+        Assert.assertEquals(3, balancedBST.Root.RightChild.LeftChild.RightChild.Level);
+        Assert.assertEquals(true, balancedBST.IsBalanced(balancedBST.Root));
+    }
+
+    @Test
+    public void testImbalanced() {
+        BSTNode node = new BSTNode(6, null);
+        BSTNode nL = new BSTNode(4, node);
+        node.LeftChild = nL;
+        BSTNode nLL = new BSTNode(2, nL);
+        nL.LeftChild = nLL;
+        BalancedBST balancedBST = new BalancedBST();
+        balancedBST.Root = node;
+        Assert.assertEquals(false, balancedBST.IsBalanced(node));
+        Assert.assertEquals(true, balancedBST.IsBalanced(nL));
+        BSTNode nR = new BSTNode(8, node);
+        node.RightChild = nR;
+        Assert.assertEquals(true, balancedBST.IsBalanced(node));
+        nLL.RightChild = nR;
+        Assert.assertEquals(false, balancedBST.IsBalanced(node));
+    }
+
+    @Test
+    public void testSame(){
+        int[] input = {1, 1, 2, 2};
+        BalancedBST balancedBST = new BalancedBST();
+        Assert.assertEquals(null, balancedBST.Root);
+        balancedBST.GenerateTree(input);
+        Assert.assertEquals(2, balancedBST.Root.RightChild.NodeKey);
+        Assert.assertEquals(1, balancedBST.Root.LeftChild.RightChild.NodeKey);
+        Assert.assertEquals(true, balancedBST.IsBalanced(balancedBST.Root));
+    }
+
+}
